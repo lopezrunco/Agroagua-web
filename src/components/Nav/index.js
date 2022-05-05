72843b9bbd58c2798cdb919e61289ffced2681ea
Damián López <lopezrunco@gmail.com>
2022-05-05 11:25:00 +0530

    Basic component Nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,10 +1,25 @@
+import { useState } from 'react'
+import './style.scss'
+
 export default function Nav() {
+    const [isNavExpanded, setIsNavExpanded] = useState(false)
+
     return (
-        <nav>
-            <a href="/">Inicio</a>
-            <a href="/servicios">Servicios</a>
-            <a href="/trabajos">Trabajos</a>
-            <a href="/contacto">Contacto</a>
+        <nav className='navigation'>
+            <a href='/' className='brand-name'>Agroagua</a>
+            <button className='hamburger' onClick={() => setIsNavExpanded(!isNavExpanded)}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="white">
+                    <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clipRule="evenodd"/>
+                </svg>
+            </button>
+            <div className={isNavExpanded ? 'navigation-menu expanded' : 'navigation-menu'}>
+                <ul>
+                    <li><a href='/'>Inicio</a></li>
+                    <li><a href="/servicios">Servicios</a></li>
+                    <li><a href="/trabajos">Trabajos</a></li>
+                    <li><a href="/contacto">Contacto</a></li>
+                </ul>
+            </div>
         </nav>
     )
 }
