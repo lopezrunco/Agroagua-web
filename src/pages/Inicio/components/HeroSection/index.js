fec32af6d2828a58579387a60ea8c7a0361b80d9
Damián López <lopezrunco@gmail.com>
2022-05-06 11:04:56 +0530

    Final styles on Nav & HeroSection components

--- src/pages/Inicio/components/HeroSection/index.js
+++ src/pages/Inicio/components/HeroSection/index.js
@@ -1,21 +1,27 @@
 import { heroSectionData } from '../../../../data'
+import borderImg from '../../../../assets/border-hero-img.png'
 import './style.scss'
 
 export default function HeroSection() {
     return (
+        <>
         <div className="hero-section">
             <div className='overcolor'>
                 <div className="container f-column">
                     <div className="content-wrapper f-column">
                         <h1>{heroSectionData.title}<span>{heroSectionData.titleSpan}</span></h1>
                         <p>{heroSectionData.text}</p>
-                        <div className='f-row'>
-                            <a href={heroSectionData.mainButtonLink}>{heroSectionData.mainButton}</a>
-                            <a href={heroSectionData.secondaryButtonLink}>{heroSectionData.secondaryButton}</a>
+                        <div className='button-group'>
+                            <a className='button button-lg primary-button' href={heroSectionData.mainButtonLink}>{heroSectionData.mainButton}</a>
+                            <a className='button button-lg secondary-button' href={heroSectionData.secondaryButtonLink}>{heroSectionData.secondaryButton}</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <div className='curvy-border'>
+            <img src={borderImg} alt='Agro agua' />
+        </div>
+        </>
     )
 }
