93c4417b369558d629badeac222c293fc8a5bcd7
Damián López <lopezrunco@gmail.com>
2022-05-07 12:22:02 +0530

    Final styles Inicio/LatestWorks

--- src/components/LastsWorks/index.js
+++ src/components/LastsWorks/index.js
@@ -1,20 +1,25 @@
 import { lastsWorks } from '../../data'
+import LatestWorkItem from '../LatestWorkItem'
 import './style.scss'
 
 export default function LastsWorks() {
     return (
-        <>
-            <div className='lasts-works-home-grid'>
-                <div className='container'>
-                    <div className='row content-wrapper'>
-                        {lastsWorks.map(({ imgUrl, id }) => (
-                            <div className='col-lg-4 item-wrapper' key={id}>
-                                <img src={imgUrl} alt='Agroagua trabajos' />
-                            </div>
-                        ))}
+        <section className='lasts-works-home-grid'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-12'>
+                        <div className='section-title'>
+                            <h2 className='title'>Últimos trabajos</h2>
+                            <h3 className='subtitle'>¿Qué hacemos?</h3>
+                        </div>
                     </div>
                 </div>
+                <div className='row content-wrapper'>
+                    {lastsWorks.map(({ imgUrl, id }, index) => (
+                        index < 3 && <LatestWorkItem imgSource={imgUrl} key={id} />
+                    ))}
+                </div>
             </div>
-        </>
+        </section>
     )
 }
---
+++ src/components/LatestWorkItem/index.js
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './style.scss'
+
+export default function LatestWorkItem(props) {
+    const [openImage, setOpenImage] = useState(false)
+    const handleClick = () => setOpenImage(!openImage)
+    openImage ? document.body.style.overflow = "hidden" : document.body.style.overflow = "scroll"
+
+    return (
+        <>
+            <div className='col-lg-4 last-work-item-wrapper' onClick={handleClick}>
+                <img src={props.imgSource} alt='Agroagua trabajos' />
+            </div>
+            {openImage && (
+                <div className='image-viewer' onClick={handleClick}>
+                    <div className='container'>
+                        <div className='img-wrapper'>
+                            <img src={props.imgSource} alt={'Agro agua trabajos'} />
+                            <i className="fas fa-times-circle close-icon"></i>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    )
+}
